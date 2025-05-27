011a1830884147d42abfe7409a57d24c552fe802
carmonae <edward.a.carmona@gmail.com>
2025-05-27 19:40:44 -0400

    fix path to folders that moved

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -10,12 +10,12 @@ import { AgentsComponent } from './agents/agents.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { BestSellingProductComponent } from './authors/best-selling-product/best-selling-product.component';
 import { CommonDetailsComponent } from './authors/common-details/common-details.component';
-import { CurrentBalanceComponent } from './authors/current-balance/current-balance.component';
-import { ProfileGrettingComponent } from './authors/profile-gretting/profile-gretting.component';
 import { SalesSummaryComponent } from './authors/sales-summary/sales-summary.component';
 import { TrackOrderComponent } from './authors/track-order/track-order.component';
 import { WeeklyVisitorsComponent } from './authors/weekly-visitors/weekly-visitors.component';
+import { CurrentBalanceComponent } from './current-balance/current-balance.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { ProfileGrettingComponent } from './profile-gretting/profile-gretting.component';
 import { ReadersComponent } from './readers/readers.component';
 
 import { SortableDirective } from 'src/app/shared/directive/sortable.directive';
