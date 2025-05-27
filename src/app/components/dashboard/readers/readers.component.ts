68815c9b1c621414425cf9ce67567d32839f5309
carmonae <edward.a.carmona@gmail.com>
2025-05-27 19:41:36 -0400

    add readers component

--- src/app/components/dashboard/readers/readers.component.ts
+++ src/app/components/dashboard/readers/readers.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '@app/auth/service/auth.keycloak.service';
+import { CurrentBalanceComponent } from '../current-balance/current-balance.component';
+import { ProfileGrettingComponent } from '../profile-gretting/profile-gretting.component';
 @Component({
-    selector: 'app-readers',
-    templateUrl: './readers.component.html',
-    styleUrls: ['./readers.component.scss'],
-    standalone: true
+  selector: 'app-readers',
+  templateUrl: './readers.component.html',
+  styleUrls: ['./readers.component.scss'],
+  standalone: true,
+  imports: [ProfileGrettingComponent, CurrentBalanceComponent],
 })
 export class ReadersComponent {
-
+  constructor(private authService: AuthService) {}
 }
