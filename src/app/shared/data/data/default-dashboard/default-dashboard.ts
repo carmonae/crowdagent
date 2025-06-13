1c25778b84acd933c1f11d35723270ab5b73adea
carmonae <edward.a.carmona@gmail.com>
2025-06-13 09:36:44 -0400

    added sample data for default dashboard

--- src/app/shared/data/data/default-dashboard/default-dashboard.ts
+++ src/app/shared/data/data/default-dashboard/default-dashboard.ts
@@ -1,73 +1,207 @@
 export interface commonDetails {
-    id: number;
-    class: string;
-    title: string,
-    data: string,
-    img: string,
-    icon: string,
-    iconClass: string;
-    profit: string,
-    profitColor: string,
-    progress: string;
-    progressColor: string;
-    isCollasped: boolean;
+  id: number;
+  class: string;
+  title: string;
+  data: string;
+  img: string;
+  icon: string;
+  iconClass: string;
+  profit: string;
+  profitColor: string;
+  progress: string;
+  progressColor: string;
+  isCollasped: boolean;
 }
 export const titleImpressionsData: commonDetails = {
-    id: 1,
-    class: 'profit-card',
-    title: 'Title Impressions',
-    data: '6542',
-    img: 'assets/images/dashboard-2/total-icon/users.png',
-    icon: 'sort-desc',
-    iconClass: 'down',
-    profit: '- 120.1%',
-    profitColor: 'secondary',
-    progress: '3%',
-    progressColor: 'primary',
-    isCollasped: false
-}
+  id: 1,
+  class: 'profit-card',
+  title: 'Title Impressions',
+  data: '6542',
+  img: 'assets/images/dashboard-2/total-icon/users.png',
+  icon: 'sort-desc',
+  iconClass: 'down',
+  profit: '- 120.1%',
+  profitColor: 'secondary',
+  progress: '3%',
+  progressColor: 'primary',
+  isCollasped: false,
+};
 
 export const titlePiquesData: commonDetails = {
-    id: 1,
-    class: 'profit-card',
-    title: 'Title PiQues',
-    data: '6542',
-    img: 'assets/images/dashboard-2/total-icon/users.png',
-    icon: 'sort-desc',
-    iconClass: 'down',
-    profit: '- 12.32%',
-    profitColor: 'secondary',
-    progress: '35%',
-    progressColor: 'primary',
-    isCollasped: false
-}
+  id: 1,
+  class: 'profit-card',
+  title: 'Title PiQues',
+  data: '6542',
+  img: 'assets/images/dashboard-2/total-icon/users.png',
+  icon: 'sort-desc',
+  iconClass: 'down',
+  profit: '- 12.32%',
+  profitColor: 'secondary',
+  progress: '35%',
+  progressColor: 'primary',
+  isCollasped: false,
+};
 
 export const abstractPiquesData: commonDetails = {
-    id: 1,
-    class: 'orders-card',
-    title: 'Abstract PiQues',
-    data: '659',
-    img: 'assets/images/dashboard-2/total-icon/order.png',
-    icon: 'sort-up',
-    iconClass: 'up',
-    profit: '+ 12.32%',
-    profitColor: 'primary',
-    progress: '45%',
-    progressColor: 'secondary',
-    isCollasped: false
-}
+  id: 1,
+  class: 'orders-card',
+  title: 'Abstract PiQues',
+  data: '659',
+  img: 'assets/images/dashboard-2/total-icon/order.png',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'primary',
+  progress: '45%',
+  progressColor: 'secondary',
+  isCollasped: false,
+};
 
 export const manuscriptsPiquesData: commonDetails = {
-    id: 1,
-    class: 'products-card',
-    title: 'Manuscript PiQues',
-    data: '319',
-    img: 'assets/images/dashboard-2/total-icon/product.png',
-    icon: 'sort-up',
-    iconClass: 'up',
-    profit: '+ 12.32%',
-    profitColor: 'dark',
-    progress: '65%',
-    progressColor: 'dark',
-    isCollasped: false
-}
+  id: 1,
+  class: 'products-card',
+  title: 'Manuscript PiQues',
+  data: '319',
+  img: 'assets/images/dashboard-2/total-icon/product.png',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'dark',
+  isCollasped: false,
+};
+
+export const readersData: commonDetails = {
+  id: 1,
+  class: 'products-card',
+  title: 'Total Readers',
+  data: '319',
+  img: 'read-book-alt',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'primary',
+  isCollasped: false,
+};
+
+export const authorsData: commonDetails = {
+  id: 1,
+  class: 'products-card',
+  title: 'Total Authors',
+  data: '319',
+  img: 'fountain-pen',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'primary',
+  isCollasped: false,
+};
+
+export const booksData: commonDetails = {
+  id: 1,
+  class: 'products-card',
+  title: 'Total Books',
+  data: '319',
+  img: 'book-alt',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'primary',
+  isCollasped: false,
+};
+
+export const litAgentsData: commonDetails = {
+  id: 1,
+  class: 'products-card',
+  title: 'Total Literary Agents',
+  data: '319',
+  img: 'book-alt',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'secondary',
+  isCollasped: false,
+};
+export const publishersData: commonDetails = {
+  id: 1,
+  class: 'products-card',
+  title: 'Total Publishers',
+  data: '319',
+  img: 'book-alt',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'secondary',
+  isCollasped: false,
+};
+
+export const freelancersData: commonDetails = {
+  id: 1,
+  class: 'products-card',
+  title: 'Total Freelancers',
+  data: '319',
+  img: 'book-alt',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'secondary',
+  isCollasped: false,
+};
+
+export const impressionsData: commonDetails = {
+  id: 1,
+  class: 'products-card',
+  title: 'Total Impressions',
+  data: '319',
+  img: 'book-alt',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'info',
+  isCollasped: false,
+};
+
+export const ratingsData: commonDetails = {
+  id: 1,
+  class: 'products-card',
+  title: 'Total Ratings',
+  data: '319',
+  img: 'book-alt',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'info',
+  isCollasped: false,
+};
+
+export const piquesData: commonDetails = {
+  id: 1,
+  class: 'products-card',
+  title: 'Total Titles piQued',
+  data: '319',
+  img: 'book-alt',
+  icon: 'sort-up',
+  iconClass: 'up',
+  profit: '+ 12.32%',
+  profitColor: 'dark',
+  progress: '65%',
+  progressColor: 'info',
+  isCollasped: false,
+};
